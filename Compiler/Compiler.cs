c1d729df0b031ebf94d399ff0c0d504409e5e551
larunnel161 <larunnel161@gmail.com>
2024-02-26 21:36:09 -0600

    1. Matching one symbols in the lexer 2. Adding a 'column' attribute in tokens

--- Compiler/Compiler.cs
+++ Compiler/Compiler.cs
@@ -1,8 +1,12 @@
+using CompilerProj.Lex;
+
 namespace CompilerProj;
 
 public class Compiler {
     
     public static void compileFile(string filePath) {
-        
+        StreamReader sr = new StreamReader(filePath);
+        Lexer lexer = new Lexer();
+        lexer.lexProgram(sr.ReadToEnd());
     }
 }
