c08b7eda5a3dec4c2019bc1f9b62463bbdc59f08
larunnel161 <larunnel161@gmail.com>
2024-03-01 12:40:07 -0600

    Importing changes from 'AssemblySimulator' repo

--- Runtime/Program.cs
+++ Runtime/Program.cs
@@ -1,13 +1,17 @@
 ﻿using Compiler.Runtime;
 
-int startProgramAddress = 0;
+// #################################################################
+// CHANGE THESE TWO VARIABLES
+
 string assemblyFilePath = "./AssemblyTests/loopGreaterThan.asm";
 string binFilePath = "./AssemblyTests/Output/loopGreaterThan.out";
 
+// #################################################################
+
+int startProgramAddress = 0;
 Assembler assembler = new Assembler(startProgramAddress);
 assembler.assembleFile(assemblyFilePath, binFilePath);
 
 Machine machine = new Machine(startProgramAddress);
 machine.loadProgram(binFilePath);
 machine.runProgram();
-Console.WriteLine();
