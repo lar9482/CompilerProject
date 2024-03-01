7fa5492a7c86762e181e1c4a4928fa73e453a9a6
lar9482 <lar9482@utulsa.edu>
2024-02-29 23:26:46 -0600

    Moving a 'ProcedureCall' into 'MultiCall'

--- Compiler/AST/Stmt/MultiCallAssign.cs
+++ Compiler/AST/Stmt/MultiCallAssign.cs
@@ -1,17 +1,14 @@
 public class MultiCallAST : StmtAST {
     public List<string> variableNames;
-    public string procedureName;
-    public List<ExprAST> args;
+    public ProcedureCallAST call;
 
     public MultiCallAST(
         List<string> variableNames,
-        string procedureName,
-        List<ExprAST> args,
+        ProcedureCallAST call,
         int lineNumber, int columnNumber
     ) : base(lineNumber, columnNumber) {
-        
+
         this.variableNames = variableNames;
-        this.procedureName = procedureName;
-        this.args = args;
+        this.call = call;
     }
 }
