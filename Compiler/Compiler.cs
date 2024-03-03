e33dab78cea51a5d5bbce66e56fc885f76e1fd9c
larunnel161 <larunnel161@gmail.com>
2024-03-03 12:49:43 -0600

    Making sure the StreamReader closes, so no weird bugs happen

--- Compiler/Compiler.cs
+++ Compiler/Compiler.cs
@@ -8,9 +8,11 @@ public class Compiler {
     
     public static void compileFile(string filePath) {
         StreamReader sr = new StreamReader(filePath);
-
+        string programText = sr.ReadToEnd();
+        sr.Close();
+        
         Lexer lexer = new Lexer();
-        Queue<Token> tokenQueue = lexer.lexProgram(sr.ReadToEnd());
+        Queue<Token> tokenQueue = lexer.lexProgram(programText);
 
         Parser parser = new Parser(tokenQueue);
         parser.parseProgram();
