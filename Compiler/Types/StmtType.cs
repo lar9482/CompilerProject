aa922c21353f6f1aca072f03de135930a8ac63aa
lar9482 <lar9482@utulsa.edu>
2024-03-21 11:24:32 -0500

    Specifying types that will be used to annotate statements

--- Compiler/Types/StmtType.cs
+++ Compiler/Types/StmtType.cs
@@ -1,5 +1,18 @@
 namespace CompilerProj.Types;
 
 internal abstract class StmtType : LangType {
-    
+}
+
+/*
+ * Unit types refer to statements that don't return anything, and the next statement can be executed
+ */
+internal sealed class UnitType : StmtType {
+    internal override string TypeTag => "unit";
+}
+
+/*
+ * Terminates types refer to statements that don't return anything, and the next statement can not be executed.
+ */
+internal sealed class TerminateType : StmtType {
+    internal override string TypeTag => "void";
 }
