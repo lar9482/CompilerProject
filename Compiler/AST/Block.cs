a654f5b7820dbbc3f28957416646ef96e0d4d7f9
lar9482 <lar9482@utulsa.edu>
2024-03-23 14:41:53 -0500

    Placing var, multi-var, array, and multiDimArray Decls into the same list for program and block nodes 1. Adjusting the parser to work with these single lists 2. Adjusting the code lifting visitor for parser testing

--- Compiler/AST/Block.cs
+++ Compiler/AST/Block.cs
@@ -3,28 +3,18 @@ using CompilerProj.Visitors;
 using CompilerProj.Context;
 
 public sealed class BlockAST : NodeAST {
-    public List<VarDeclAST> varDecls;
-    public List<MultiVarDeclAST> multiVarDecls;
-    public List<ArrayDeclAST> arrays;
-    public List<MultiDimArrayDeclAST> multiDimArrays;
-
+    public List<DeclAST> declarations;
     public List<StmtAST> statements;
 
     public SymbolTable? scope;
 
     public BlockAST(
-        List<VarDeclAST> varDecls,
-        List<MultiVarDeclAST> multiVarDecls,
-        List<ArrayDeclAST> arrays,
-        List<MultiDimArrayDeclAST> multiDimArrays,
+        List<DeclAST> declarations,
         List<StmtAST> statements, 
         int lineNumber, int columnNumber
     ) : base(lineNumber, columnNumber) {
         
-        this.varDecls = varDecls;
-        this.multiVarDecls = multiVarDecls;
-        this.arrays = arrays;
-        this.multiDimArrays = multiDimArrays;
+        this.declarations = declarations;
         this.statements = statements;
     }
 
