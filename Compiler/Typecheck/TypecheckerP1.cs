86bcf9190239a674807c9f229db2d0ecc34cb327
lar9482 <lar9482@utulsa.edu>
2024-03-24 23:47:43 -0500

    Defining a new Visitor for actual typechecking

--- Compiler/Typecheck/TypecheckerP1.cs
+++ Compiler/Typecheck/TypecheckerP1.cs
@@ -1,4 +1,3 @@
-using System.Reflection.Metadata;
 using CompilerProj.Context;
 using CompilerProj.Visitors;
 
--- Compiler/Visitors/ASTVisitor.cs
+++ Compiler/Visitors/ASTVisitor.cs
@@ -22,15 +22,16 @@ public interface ASTVisitor {
     public void visit(ConditionalAST conditional);
     public void visit(WhileLoopAST whileLoop);
     public void visit(ReturnAST returnStmt);
-    public void visit(FunctionCallAST functionCall);
-
+    public void visit(ProcedureCallAST procedureCall);
+    
+    
     // Expr Nodes
     public void visit(BinaryExprAST binaryExpr);
     public void visit(UnaryExprAST unaryExpr);
     public void visit(VarAccessAST varAccess);
     public void visit(ArrayAccessAST arrayAccess);
     public void visit(MultiDimArrayAccessAST multiDimArrayAccess);
-    public void visit(ProcedureCallAST procedureCall);
+    public void visit(FunctionCallAST functionCall);
     public void visit(IntLiteralAST intLiteral);
     public void visit(BoolLiteralAST boolLiteral);
     public void visit(CharLiteralAST charLiteral);
