d5026fec3839eb5e824ee0fb0805b6299d3f29ab
larunnel161 <larunnel161@gmail.com>
2024-03-24 23:47:43 -0500

    Defining a new Visitor for actual typechecking

---
+++ Compiler/Typecheck/TypecheckerP2.cs
@@ -0,0 +1,40 @@
+using CompilerProj.Visitors;
+
+/*
+ * This pass does the actual full typechecking, via recursive visits and ASt annotation.
+ */
+public sealed class TypecheckerP2 : ASTVisitor {
+    // Top level nodes
+    public void visit(ProgramAST program) { }
+    public void visit(VarDeclAST varDecl) { }
+    public void visit(MultiVarDeclAST multiVarDecl) { }
+    public void visit(ArrayDeclAST array) { }
+    public void visit(MultiDimArrayDeclAST multiDimArray) { }
+    public void visit(FunctionAST function) { }
+    public void visit(ParameterAST parameter) { }
+    public void visit(BlockAST block) { }
+
+    // Stmt nodes
+    public void visit(AssignAST assign) { }
+    public void visit(MultiAssignAST multiAssign) { }
+    public void visit(MultiAssignCallAST multiAssignCall) { }
+    public void visit(ArrayAssignAST arrayAssign) { }
+    public void visit(MultiDimArrayAssignAST multiDimArrayAssign) { }
+    public void visit(ConditionalAST conditional) { }
+    public void visit(WhileLoopAST whileLoop) { }
+    public void visit(ReturnAST returnStmt) { }
+    public void visit(ProcedureCallAST procedureCall) { }
+    
+    
+    // Expr Nodes
+    public void visit(BinaryExprAST binaryExpr) { }
+    public void visit(UnaryExprAST unaryExpr) { }
+    public void visit(VarAccessAST varAccess) { }
+    public void visit(ArrayAccessAST arrayAccess) { }
+    public void visit(MultiDimArrayAccessAST multiDimArrayAccess) { }
+    public void visit(FunctionCallAST functionCall) { }
+    public void visit(IntLiteralAST intLiteral) { }
+    public void visit(BoolLiteralAST boolLiteral) { }
+    public void visit(CharLiteralAST charLiteral) { }
+    public void visit(StrLiteralAST strLiteral) { }
+}
