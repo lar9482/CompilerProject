c1175af386274d00b56e7a0fc14ce28a22eba7fa
lar9482 <lar9482@utulsa.edu>
2024-03-27 01:35:02 -0500

    Instead of 'type' in Expr being null, it is set to UninitializedType. It seems to be easier to work with honestly

--- Compiler/AST/Expr/Expr.cs
+++ Compiler/AST/Expr/Expr.cs
@@ -2,8 +2,9 @@ using CompilerProj.AST;
 
 public abstract class ExprAST : NodeAST {
     
-    public SimpleType? type;
+    public SimpleType type;
     
     public ExprAST(int lineNumber, int columnNumber) : base(lineNumber, columnNumber) {
+        this.type = new UninitializedType();
     }
 }
