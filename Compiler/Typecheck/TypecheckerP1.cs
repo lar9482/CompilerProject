cf372a2c2aeb05a46b9182af205f9f9341540f7e
larunnel161 <larunnel161@gmail.com>
2024-03-28 11:03:39 -0500

    Moving the initialization and checking declarations to the base class

--- Compiler/Typecheck/TypecheckerP1.cs
+++ Compiler/Typecheck/TypecheckerP1.cs
@@ -2,12 +2,75 @@ using CompilerProj.Context;
 using CompilerProj.Visitors;
 
 /*
- * This pass will record the symbols of the top level declarations
+ * This pass will record the symbols of the top level declarations.
  */
 public sealed class TypecheckerP1 : TypeChecker {
     public TypecheckerP1() { 
     }
 
+    public override void visit(ProgramAST program) { 
+        context.push();
+
+        foreach(DeclAST decl in program.declarations) {
+            decl.accept(this);
+        }
+
+        foreach(FunctionAST function in program.functions) {
+            function.accept(this);
+        }
+
+        // Future system call support.
+        addIO();
+        addConv();
+        program.scope = context.pop();
+    }
+
+    public override void visit(VarDeclAST varDecl) { 
+        initializeVarDecl(varDecl);
+    }
+
+    public override void visit(MultiVarDeclAST multiVarDecl) { 
+        initializeMultiVarDecl(multiVarDecl);
+    }
+
+    public override void visit(MultiVarDeclCallAST multiVarDeclCall) {
+        initializeMultiVarDeclCall(multiVarDeclCall);
+    }
+
+    public override void visit(ArrayDeclAST array) { 
+        initializeArrayDecl(array);
+    }
+
+    public override void visit(MultiDimArrayDeclAST multiDimArray) { 
+        initializeMultiDimArrayDecl(multiDimArray);
+    }
+
+    public override void visit(FunctionAST function) { 
+        if (context.lookup(function.name) != null) {
+            errorMsgs.Add(
+                String.Format(
+                    "{0}:{1} SemanticError: {2} exists already.",
+                    function.lineNumber,
+                    function.columnNumber,
+                    function.name
+                )
+            );
+            return;
+        }
+
+        List<SimpleType> parameterTypes = new List<SimpleType>();
+        foreach(ParameterAST param in function.parameters) {
+            parameterTypes.Add(param.type);
+        }
+
+        SymbolFunction symbolFunc = new SymbolFunction(
+            function.name,
+            parameterTypes.ToArray<SimpleType>(),
+            function.returnTypes.ToArray<SimpleType>()
+        );
+        context.put(function.name, symbolFunc);
+    }
+    
     private void addIO() {
         // Reads a string to standard output
         context.put("print", new SymbolFunction(
@@ -84,147 +147,6 @@ public sealed class TypecheckerP1 : TypeChecker {
         ));
     }
 
-    public override void visit(ProgramAST program) { 
-        context.push();
-
-        foreach(DeclAST decl in program.declarations) {
-            decl.accept(this);
-        }
-
-        foreach(FunctionAST function in program.functions) {
-            function.accept(this);
-        }
-
-        // Future system call support.
-        addIO();
-        addConv();
-        program.scope = context.pop();
-    }
-
-    public override void visit(VarDeclAST varDecl) { 
-        if (context.lookup(varDecl.name) != null) {
-            errorMsgs.Add(
-                String.Format(
-                    "{0}:{1} SemanticError: {2} exists already.", 
-                    varDecl.lineNumber, varDecl.columnNumber, varDecl.name
-                )
-            );
-            return;
-        }
-
-        SymbolVariable symbolVar = new SymbolVariable(
-            varDecl.name, varDecl.declType
-        );
-
-        context.put(varDecl.name, symbolVar);
-    }
-
-    public override void visit(MultiVarDeclAST multiVarDecl) { 
-        foreach(KeyValuePair<string, PrimitiveType> nameAndType in multiVarDecl.declTypes) {
-            if (context.lookup(nameAndType.Key) != null) {
-                errorMsgs.Add(
-                    String.Format(
-                        "{0}:{1} SemanticError:{2} exists already.", 
-                        multiVarDecl.lineNumber, multiVarDecl.columnNumber, nameAndType.Key
-                    )
-                );
-                continue;
-            }
-
-            SymbolVariable symbolVar = new SymbolVariable(
-                nameAndType.Key,
-                nameAndType.Value
-            );
-
-            context.put(nameAndType.Key, symbolVar);
-        }
-    }
-
-    public override void visit(MultiVarDeclCallAST multiVarDeclCall) {
-        foreach(KeyValuePair<string, PrimitiveType> nameAndType in multiVarDeclCall.declTypes) {
-            if (context.lookup(nameAndType.Key) != null) {
-                errorMsgs.Add(
-                    String.Format(
-                        "{0}:{1} SemanticError: {2} exists already.", 
-                        multiVarDeclCall.lineNumber, multiVarDeclCall.columnNumber, nameAndType.Key
-                    )
-                );
-                continue;
-            }
-
-            SymbolVariable symbolVar = new SymbolVariable(
-                nameAndType.Key,
-                nameAndType.Value
-            );
-
-            context.put(nameAndType.Key, symbolVar);
-        }
-    }
-
-    public override void visit(ArrayDeclAST array) { 
-        if (context.lookup(array.name) != null) {
-            errorMsgs.Add(
-                String.Format(
-                    "{0}:{1} SemanticError: {2} exists already.", 
-                    array.lineNumber, array.columnNumber, array.name
-                )
-            );
-            return;
-        }
-
-        SymbolVariable symbolVar = new SymbolVariable(
-            array.name,
-            array.declType
-        );
-
-        context.put(array.name, symbolVar);
-    }
-
-    public override void visit(MultiDimArrayDeclAST multiDimArray) { 
-        if (context.lookup(multiDimArray.name) != null) {
-            errorMsgs.Add(
-                String.Format(
-                    "{0}:{1} SemanticError: {2} exists already.", 
-                    multiDimArray.lineNumber, multiDimArray.columnNumber, multiDimArray.name
-                )
-            );
-            return;
-        }
-
-        SymbolVariable symbolVar = new SymbolVariable(
-            multiDimArray.name,
-            multiDimArray.declType
-        );
-
-        context.put(multiDimArray.name, symbolVar);
-    }
-
-    public override void visit(FunctionAST function) { 
-        if (context.lookup(function.name) != null) {
-            errorMsgs.Add(
-                String.Format(
-                    "{0}:{1} SemanticError: {2} exists already.",
-                    function.lineNumber,
-                    function.columnNumber,
-                    function.name
-                )
-            );
-            return;
-        }
-
-        List<SimpleType> parameterTypes = new List<SimpleType>();
-        foreach(ParameterAST param in function.parameters) {
-            parameterTypes.Add(param.type);
-        }
-
-        SymbolFunction symbolFunc = new SymbolFunction(
-            function.name,
-            parameterTypes.ToArray<SimpleType>(),
-            function.returnTypes.ToArray<SimpleType>()
-        );
-        context.put(function.name, symbolFunc);
-    }
-    
     //Unused visit procedures
     public override void visit(ParameterAST parameter) { throw new NotImplementedException("This visit is not used"); }
     public override void visit(BlockAST block) { throw new NotImplementedException("This visit is not used"); }
--- Compiler/Typecheck/TypecheckerP2.cs
+++ Compiler/Typecheck/TypecheckerP2.cs
@@ -29,228 +29,31 @@ public sealed class TypecheckerP2 : TypeChecker {
     public override void visit(VarDeclAST varDecl) { 
         varDecl.type = new UnitType();
 
-        SymbolVariable varSymbol = (SymbolVariable) lookUpSymbolFromContext(
-            varDecl.name, varDecl.lineNumber, varDecl.columnNumber
-        );
-
-        if (varDecl.initialValue == null) {
-            return;
-        }
-        varDecl.initialValue.accept(this);
-        SimpleType initialValueType = varDecl.initialValue.type;
-        SimpleType varDeclType = varSymbol.type;
-
-        if (!sameTypes(initialValueType, varDeclType)) {
-            errorMsgs.Add(
-                String.Format(
-                    "{0}:{1} SemanticError: The initial value type, {2}, doesn't match with the variable declaration.",
-                    varDecl.initialValue.lineNumber,
-                    varDecl.initialValue.columnNumber,
-                    simpleTypeToString(initialValueType)
-                )
-            );
-        }
+        checkVarDecl(varDecl);
     }
 
     public override void visit(MultiVarDeclAST multiVarDecl) { 
         multiVarDecl.type = new UnitType();
 
-        foreach(string varName in multiVarDecl.names) {
-            SymbolVariable varSymbol = (SymbolVariable) lookUpSymbolFromContext(
-                varName, multiVarDecl.lineNumber, multiVarDecl.columnNumber
-            );
-
-            ExprAST? initialValue = multiVarDecl.initialValues[varName];
-            if (initialValue == null) {
-                continue;
-            }
-
-            initialValue.accept(this);
-            SimpleType initialValueType = initialValue.type;
-
-            if (!sameTypes(initialValueType, varSymbol.type)) {
-                errorMsgs.Add(
-                    String.Format(
-                        "{0}:{1} SemanticError: The initial value type, {2}, doesn't match with the variable declaration.",
-                        initialValue.lineNumber,
-                        initialValue.columnNumber,
-                        simpleTypeToString(initialValueType)
-                    )
-                );
-            }
-        }
+        checkMultiVarDecl(multiVarDecl);
     }
 
     public override void visit(MultiVarDeclCallAST multiVarDeclCallAST) {
         multiVarDeclCallAST.type = new UnitType();
 
-        SymbolFunction symbolFunction = (SymbolFunction) lookUpSymbolFromContext(
-            multiVarDeclCallAST.functionCall.functionName,
-            multiVarDeclCallAST.lineNumber, multiVarDeclCallAST.columnNumber
-        );
-
-        for (int i = 0; i < multiVarDeclCallAST.names.Count; i++) {
-            string varName = multiVarDeclCallAST.names[i];
-            SymbolVariable symbolVar = (SymbolVariable) lookUpSymbolFromContext(
-                varName, multiVarDeclCallAST.lineNumber, multiVarDeclCallAST.columnNumber
-            );
-
-            SimpleType variableType = symbolVar.type;
-            SimpleType returnFuncType = symbolFunction.returnTypes[i];
-            if (!sameTypes(variableType , returnFuncType)) {
-                errorMsgs.Add(
-                    String.Format(
-                        "{0}:{1} SemanticError: {2}'s type doesn't match with the return type {3}",
-                        multiVarDeclCallAST.lineNumber,
-                        multiVarDeclCallAST.columnNumber,
-                        varName, simpleTypeToString(returnFuncType)
-                    )
-                );
-            }
-        }
-
-        FunctionCallAST functionCall = multiVarDeclCallAST.functionCall;
-        for (int i = 0; i < functionCall.args.Count; i++) {
-            ExprAST param = functionCall.args[i];
-            param.accept(this);
-
-            SimpleType paramType = param.type;
-            SimpleType expectedParamType = symbolFunction.parameterTypes[i];
-            
-            if (!sameTypes(paramType, expectedParamType)) {
-                errorMsgs.Add(
-                    String.Format(
-                        "{0}:{1} SemanticError: Param's type {2} doesn't match with the expected type {3}",
-                        param.lineNumber, param.columnNumber,
-                        simpleTypeToString(paramType), simpleTypeToString(expectedParamType)
-                    )
-                );
-            }
-        }
+        checkMultiVarDeclCall(multiVarDeclCallAST);
     }
 
     public override void visit(ArrayDeclAST array) { 
         array.type = new UnitType();
 
-        array.size.accept(this);
-        SimpleType arraySizeType = array.size.type;
-
-        if (arraySizeType.TypeTag != "int" && arraySizeType.TypeTag != "bool") {
-            errorMsgs.Add(
-                String.Format(
-                    "{0}:{1} SemanticError: The size expression must be an int or a bool",
-                    array.size.lineNumber, array.size.columnNumber
-                )
-            );
-        }
-
-        if (array.initialValues == null) {
-            return;
-        }
-
-        SymbolVariable symbolVar = (SymbolVariable) lookUpSymbolFromContext(
-            array.name, array.lineNumber, array.columnNumber
-        );
-
-        SimpleType expectedInitValType;
-        switch(symbolVar.type) {
-            case ArrayType<PrimitiveType> arrayDecl when arrayDecl.baseType.TypeTag=="int": 
-                expectedInitValType = new IntType();
-                break;
-            case ArrayType<PrimitiveType> arrayDecl when arrayDecl.baseType.TypeTag=="bool":
-                expectedInitValType = new BoolType();
-                break;
-            default:
-                throw new Exception(
-                    String.Format(
-                        "{0}:{1} SemanticError: The array type could not be resolved incorrectly"
-                    )
-                );
-        }
-
-        foreach(ExprAST initialValue in array.initialValues) {
-            initialValue.accept(this);
-            SimpleType actualInitValType = initialValue.type;
-
-            if (!sameTypes(expectedInitValType, actualInitValType)) {
-                errorMsgs.Add(
-                    String.Format(
-                        "{0}:{1} SemanticError: The initial value must be of type {2}",
-                        initialValue.lineNumber, initialValue.columnNumber,
-                        simpleTypeToString(actualInitValType)
-                    )
-                );
-            }
-        }
+        checkArrayDecl(array);
     }
 
     public override void visit(MultiDimArrayDeclAST multiDimArray) { 
         multiDimArray.type = new UnitType();
 
-        multiDimArray.rowSize.accept(this);
-        multiDimArray.colSize.accept(this);
-
-        SimpleType rowSizeType = multiDimArray.rowSize.type;
-        SimpleType colSizeType = multiDimArray.colSize.type;
-
-        if (rowSizeType.TypeTag != "int" && rowSizeType.TypeTag != "bool") {
-            errorMsgs.Add(
-                String.Format(
-                    "{0}:{1} SemanticError: The row size expression must be an int or a bool",
-                    multiDimArray.rowSize.lineNumber, multiDimArray.rowSize.columnNumber
-                )
-            );
-        }
-        if (colSizeType.TypeTag != "int" && colSizeType.TypeTag != "bool") {
-            errorMsgs.Add(
-                String.Format(
-                    "{0}:{1} SemanticError: The column size expression must be an int or a bool",
-                    multiDimArray.colSize.lineNumber, multiDimArray.colSize.columnNumber
-                )
-            );
-        }
-
-        if (multiDimArray.initialValues == null) {
-            return;
-        }
-
-        SymbolVariable symbolVar = (SymbolVariable) lookUpSymbolFromContext(
-            multiDimArray.name, multiDimArray.lineNumber, multiDimArray.columnNumber
-        );
-
-        SimpleType expectedInitValType;
-        switch(symbolVar.type) {
-            case MultiDimArrayType<PrimitiveType> arrayDecl when arrayDecl.baseType.TypeTag=="int": 
-                expectedInitValType = new IntType();
-                break;
-            case MultiDimArrayType<PrimitiveType> arrayDecl when arrayDecl.baseType.TypeTag=="bool":
-                expectedInitValType = new BoolType();
-                break;
-            default:
-                throw new Exception(
-                    String.Format(
-                        "{0}:{1} SemanticError: The array type could not be resolved incorrectly"
-                    )
-                );
-        }
-
-        for (int i = 0; i < multiDimArray.initialValues.Length; i++) {
-            for (int j = 0; j < multiDimArray.initialValues[i].Length; j++) {
-                ExprAST initialValue = multiDimArray.initialValues[i][j];
-                initialValue.accept(this);
-
-                SimpleType actualInitValType = initialValue.type;
-                if (!sameTypes(expectedInitValType, actualInitValType)) {
-                    errorMsgs.Add(
-                        String.Format(
-                            "{0}:{1} SemanticError: The initial value must be of type {2}",
-                            initialValue.lineNumber, initialValue.columnNumber,
-                            simpleTypeToString(actualInitValType)
-                        )
-                    );
-                }
-            }
-        }
+        checkMultiDimArrayDecl(multiDimArray);
     }
 
     public override void visit(FunctionAST function) { throw new NotImplementedException("This visit is not used."); }
