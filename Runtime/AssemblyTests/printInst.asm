aa381c7950c9d3dafe69855346dfa71a699accf5
lar9482 <lar9482@utulsa.edu>
2024-04-06 15:06:51 -0500

    Moving "Runtime" to a submodule

--- Runtime/AssemblyTests/printInst.asm
+++
@@ -1,6 +0,0 @@
-main:
-
-    movI r1, 255
-    printw_int r1
-    printw_hex r1
-    printw_bin r1
--- Runtime/Enums/InterruptCommand.cs
+++
@@ -1,8 +0,0 @@
-namespace Compiler.Runtime;
-
-public enum InterruptCommand {
-    halt = 0,
-    printw_int = 1, 
-    printw_hex = 2, 
-    printw_bin = 3
-}
