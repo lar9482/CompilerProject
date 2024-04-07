a2cd8a923718e20f032fbf1976235ca470420c54
larunnel161 <larunnel161@gmail.com>
2024-04-07 18:26:46 -0500

    Writing infrastructure for an expression stack in the IR interpreter

---
+++ Compiler/IRInterpreter/Configuration.cs
@@ -0,0 +1,10 @@
+public struct IRConfiguration {
+    /** Prefix for argument registers */
+    public const string ABSTRACT_ARG_PREFIX = "_ARG";
+
+    /** Prefix for return registers */
+    public const string ABSTRACT_RET_PREFIX = "_RV";
+
+    /** Word size; assumes a 32-bit architecture */
+    public const int wordSize = 4;
+}
