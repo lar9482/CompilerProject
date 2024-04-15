af663ffc481b76c81c882ef93203519fdb872072
lar9482 <lar9482@utulsa.edu>
2024-04-14 19:09:23 -0500

    1. Generating IR for function calls in expressions. 2. Writing two tests for calling functions. 3. Writing infrastructure for the rest of the instructions in the IRSimulator.

--- CompilerTests/IRGenerationTests/IRGenerationTests.cs
+++ CompilerTests/IRGenerationTests/IRGenerationTests.cs
@@ -18,4 +18,26 @@ public class IRGenerationTests {
         int retVal = simulator.call("b", args);
         Assert.That(retVal, Is.EqualTo(12));
     }
+
+    [Test]
+    public void test2() {
+        string filePath = "../../../IRGenerationTests/ProgramFiles/test2.prgm";
+        IRCompUnit IR = Compiler.generateIR(filePath);
+        int[] args = new int[] { 10 };
+
+        IRSimulator simulator = new IRSimulator(IR);
+        int retVal = simulator.call("main", args);
+        Assert.That(retVal, Is.EqualTo(25));
+    }
+
+    [Test]
+    public void test3() {
+        string filePath = "../../../IRGenerationTests/ProgramFiles/test3.prgm";
+        IRCompUnit IR = Compiler.generateIR(filePath);
+        int[] args = new int[] { };
+
+        IRSimulator simulator = new IRSimulator(IR);
+        int retVal = simulator.call("main", args);
+        Assert.That(retVal, Is.EqualTo(600));
+    }
 }
