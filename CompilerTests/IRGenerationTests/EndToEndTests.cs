973318ed7e777363dd62bfbf5ed5651e598ffca9
larunnel161 <larunnel161@gmail.com>
2024-04-20 20:51:42 -0500

    Testing IR generation on tail/non-tail recursion.

---
+++ CompilerTests/IRGenerationTests/EndToEndTests.cs
@@ -0,0 +1,24 @@
+using CompilerProj;
+
+namespace CompilerTests;
+
+public class EndToEndTests {
+    [SetUp]
+    public void Setup() {}
+
+    [Test]
+    public void IRGen_Factorial() {
+        string filePath = "../../../IRGenerationTests/EndToEndTests/factorial.prgm";
+        IRCompUnit IR = Compiler.generateIR(filePath);
+        int[] args = new int[] {
+            5
+        };
+
+        IRSimulator simulator = new IRSimulator(IR);
+        int nonTailRetVal = simulator.call("factorial_nonTail", args);
+        int tailRetVal = simulator.call("factorial_tailWrap", args);
+        
+        Assert.That(nonTailRetVal, Is.EqualTo(120));
+        Assert.That(tailRetVal, Is.EqualTo(120));
+    }
+}
